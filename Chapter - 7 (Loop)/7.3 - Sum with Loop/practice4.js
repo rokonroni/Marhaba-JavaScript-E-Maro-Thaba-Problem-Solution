ff60ad203da9363bbb20fd13ae68b1148fad2a5f
rokonroni <rokonuzzamanroni@gmail.com>
2025-06-03 10:59:20 +0600

    Chapter - 7 (Loop) added

---
+++ Chapter - 7 (Loop)/7.3 - Sum with Loop/practice4.js
@@ -0,0 +1,12 @@
+// 8. while লুপ ব্যবহার করে 21 থেকে 50 পর্যন্ত সংখ্যাগুলোর যোগফল বের কর এবং প্রতিবারের যোগফল দেখাবি।
+ 
+let numbers = 21;
+let sum = 0;
+
+while (numbers <= 50) {
+    sum += numbers;
+    console.log(`সংখ্যা: ${numbers}, যোগফল: ${sum}`);
+    numbers++;
+}
+
+console.log(`সর্বমোট যোগফল: ${sum}`);
